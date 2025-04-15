6d6c65e6c76d9ca1054ba2721c93156b99a1c2ce
Emmanuel Fosu <emmanuel.fosuduffour@gmail.com>
2025-04-15 16:31:28 +0000

    Fix: Color contrast

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-// This is a fake server to subscribe an email address. The counter is used to reject the subscription when it is 3
+// This is a fake server to subscribe an email address. The counter is used to reject the subscription when it is 2
 let count = 0; 
 
 export default function postEmail(email: string): Promise<void> {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            if (count <= 2) {
+            if (count <= 1) {
                 resolve();
                 count++; // Increase counter
             } else {
